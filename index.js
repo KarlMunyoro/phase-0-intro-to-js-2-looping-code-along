2a6a5daf2df58e07ed5236f3851dac1e7a1ac34e
Karl Munyoro Kamau <kxrlmunyoro@gmail.com>
2025-04-14 18:44:05 +0300

    Looping Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    let env=[];
+    for(let i=0; i<array.length; i++){
+        let result = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+        env.push(result);
+    }
+    console.log(env);
+    return env;
+}
+
+writeCards(['Guadalupe', 'Ollie',  'Aki'], 'surprise');
+
+function countDown(){
+    for(let i=10; i>=0; i--){
+        console.log(i);
+    }
+}
